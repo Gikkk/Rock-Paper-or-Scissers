0d75fc7325893a85ca8503d158dbd944bb5425a0
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2020-06-19 12:52:24 +0400

    Under constraction

--- app.js
+++ app.js
@@ -63,37 +63,4 @@ startGameBtn.addEventListener('click',  () => {
 });
 
 
-
-
-    const combine = (resultHandler, operation, ...numbers) => {
-    const validateNumber = (number) => {
-      return isNaN(number) ? 0 : number;
-    };
-  
-    let sum = 0;
-    for (const num of numbers) {
-        if(operation === "ADD"){
-            sum += validateNumber(num);
-        }else{
-            sum -= num;
-        }
-    }
-    resultHandler(sum);
-  };
-  
-//     const subtractUp = function(resultHandler, ...numbers) {
-//     let sum = 0;
-//     for (const num of numbers) { // don't use that
-//       sum -= num;
-//     }
-//     resultHandler(sum);
-//   };
-  
-    const showResult = (msgText, result) => {
-    alert(msgText+ " " + result);
-  };
-  
-    combine(showResult.bind(this, "Result of adding"), "ADD", 1, 5, 'fdsa', -3, 6, 10);
-    combine(showResult.bind(this, "Result of adding"),"ADD",1, 5, 10, -3, 6, 10, 25, 88);
-    combine(showResult.bind(this, "Result of subtracting"),"SUBTRACT",1, 10, 15, 20);
-
+    
